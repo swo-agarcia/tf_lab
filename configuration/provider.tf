eb20c3d20fb3011e479c6bf8909f5807de6692a5
swo-agarcia <129551358+swo-agarcia@users.noreply.github.com>
2023-05-23 10:47:38 -0500

    Update provider.tf

--- configuration/provider.tf
+++ configuration/provider.tf
@@ -2,12 +2,21 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
+  }
+}
+
+#Backend
+backend "s3" {
+  bucket         = "s3-dev-aws-deletme1234-backend"
+  key            = "demo-lab-devsecops.tfstate"
+  region         = "us-east-1"
+  dynamodb_table = "dynamodb-dev-aws-deletme1234-backend"
+  # skip_credentials_validation = true
   }
 }
 
 provider "aws" {
   region = var.region
-  profile = "backend"
 }
