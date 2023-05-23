086b45b943034f2eb7c11981ebcce46b09124d66
swo-agarcia <129551358+swo-agarcia@users.noreply.github.com>
2023-05-23 10:23:02 -0500

    Update main.tf

--- configuration/main.tf
+++ configuration/main.tf
@@ -3,15 +3,14 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = var.vpc_id
 
   ingress {
     description      = "TLS from VPC"
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = [var.cidr_block]
   }
 
   egress {
@@ -19,7 +18,6 @@ resource "aws_security_group" "allow_tls" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
